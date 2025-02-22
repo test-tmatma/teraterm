913bf3f5eae5485058b48765b399ea3d74789948
Masaru Tsuchiyama <m.tmatma@gmail.com>
2025-02-22 16:23:59 +0900

    add log

--- cygwin/cygterm/build.bat
+++ cygwin/cygterm/build.bat
@@ -3,7 +3,9 @@ rem PATH=c:\cygwin\bin
 PATH=c:\cygwin64\bin
 dir  c:\cygwin64\bin
 uname -a > build_info.txt
+echo make -j cygterm+-i686
 make -j cygterm+-i686
+echo make -j cygterm+-x86_64
 make -j cygterm+-x86_64
 file cygterm+-i686/*.exe >> build_info.txt
 file cygterm+-x86_64/*.exe >> build_info.txt
